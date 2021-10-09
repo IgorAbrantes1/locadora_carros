c6a20bd617c195e6bf7865370494fc7c4be5d4c6
IgorAbrantes <igorlk109@hotmail.com>
2021-10-08 21:04:27 -0300

    update brandfactory

--- database/factories/Brand/BrandFactory.php
+++ database/factories/Brand/BrandFactory.php
@@ -38,7 +38,7 @@ class BrandFactory extends Factory
      */
     private function resizeImage(): string
     {
-        $image = 'https://igorabrantes.com/assets/img/profile/igor-abrantes.jpeg';
+        $image = 'https://logodownload.org/wp-content/uploads/2014/02/ford-logo-1-1.png';
         $resize = Image::make($image)->resize(300, null, function ($constraint) {
             $constraint->aspectRatio();
         })->encode('png');
